9ee7a57ff47ded4939848a97c0986022d8834cb3
ilektrym <ilektr19@gmail.com>
2023-11-19 21:11:19 +0300

    test login

--- src/test/java/com/example/Automaster/AutomasterApplicationTests.java
+++ src/test/java/com/example/Automaster/AutomasterApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example.Automaster;
 
+import com.example.Automaster.controllers.AuthController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AutomasterApplicationTests {
 
+	@Autowired
+	private AuthController controller;
 	@Test
 	void contextLoads() {
 	}
---
+++ src/test/java/com/example/Automaster/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package com.example.Automaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/" + "login",
+                String.class)).contains("Please sign in");
+    }
+}
